177f06137dc808fd93343a3b0e6adb9f433dd7e0
Viachaslau Zharko <viachaslau.zharko@gmail.com>
2020-03-13 15:57:12 +0300

    add code to JS files

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(typeof(members) !== 'object' || !Array.isArray(members)) {
+    return false;
+  }
+  
+    return members.filter(team => typeof(team) === 'string')
+                  .map((team) => {team = team.toUpperCase()
+                    for(let k = 0; k < team.length; k++) {
+                      if(team[k] >= 'A' && team[k] <= 'Z') {
+                        return team[k];
+                      }
+                    }
+                  })
+                  .sort()
+                  .join('');
+  }
+ 
+
+
+
